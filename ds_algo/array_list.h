bbee4f579dfc738f8bdd4d8aacfd21a619c269f7
Kang In Cheol <kangic21@gmail.com>
2015-05-07 22:21:55 +0900

    modify tab to space

--- ds_algo/array_list.h
+++ ds_algo/array_list.h
@@ -5,27 +5,27 @@
 
 template <typename T>
 class array_list {
-public:
-	array_list(void);
-	virtual ~array_list(void);
+ public:
+  array_list(void);
+  virtual ~array_list(void);
 
-	void add_to_front(const T& data);
-	void add_to_back(const T& data);
+  void add_to_front(const T& data);
+  void add_to_back(const T& data);
 
-	void delete_data(int pos);
+  void delete_data(int pos);
 
-	bool is_empty();
-	bool is_full();
+  bool is_empty();
+  bool is_full();
 
-	int	size();
-	const T& get_data(int pos);
+  int	size();
+  const T& get_data(int pos);
 
-	void clear();
-	void display();
+  void clear();
+  void display();
 
-private:
-	T list[MAX_SIZE];
-	int length;
+ private:
+  T list[MAX_SIZE];
+  int length;
 };
 
 #endif // _ARRAY_LIST_H_
