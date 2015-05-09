45840706796f6ee01df818c21133a199fce7f764
Kang In Cheol <kangic21@gmail.com>
2015-05-09 13:11:43 +0900

    edit tap to space

--- ds_algo/tests/test_array_list.cc
+++ ds_algo/tests/test_array_list.cc
@@ -3,30 +3,30 @@
 #include <iostream>
 
 int main(void) {
-	array_list<int> list;
+  array_list<int> list;
 
-	list.clear();
+  list.clear();
 
-	list.add_to_back(100);
-	list.add_to_back(30);
-	list.add_to_front(40);
+  list.add_to_back(100);
+  list.add_to_back(30);
+  list.add_to_front(40);
 
-	std::cout << "length = " << list.size() << std::endl;
+  std::cout << "length = " << list.size() << std::endl;
 	
-	list.add_to_front(70);
-	list.add_to_back(80);
+  list.add_to_front(70);
+  list.add_to_back(80);
 
-	std::cout << "length = " << list.size() << std::endl;
+  std::cout << "length = " << list.size() << std::endl;
 
-	list.display();
+  list.display();
 
-	int del_num = list.get_data(3);
-	list.delete_data(3);
-	std::cout << "delete = " << del_num << std::endl;
+  int del_num = list.get_data(3);
+  list.delete_data(3);
+  std::cout << "delete = " << del_num << std::endl;
 
-	std::cout << "length = " << list.size() << std::endl;
+  std::cout << "length = " << list.size() << std::endl;
 
-	list.display();
+  list.display();
 
-	return 0;
+  return 0;
 }
