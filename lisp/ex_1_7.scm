51898638f335036ebcf8df4a1f4e494224327c7f
Kang In Cheol <kangic21@gmail.com>
2015-05-11 01:09:58 +0900

    add some solutions

---
+++ lisp/ex_1_7.scm
@@ -0,0 +1,18 @@
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess ;; true
+      (sqrt-iter (improve guess x) x) ;; false
+      ))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (improve guess x) guess))
+     (* guess 0.001)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
